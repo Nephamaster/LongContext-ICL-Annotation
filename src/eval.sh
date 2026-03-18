2e785d7f0b7bb18596e5c3d827d3ff1ab3e757f3
Nephamaster <1723433721@qq.com>
2026-03-18 14:00:42 +0800

    add eval.sh

---
+++ src/eval.sh
@@ -0,0 +1,8 @@
+python main.py --task_id 1
+python main.py --task_id 2
+python main.py --task_id 3
+python main.py --task_id 4
+python main.py --task_id 5
+python main.py --task_id 6
+python main.py --task_id 7
+# python main.py --task_id 8
